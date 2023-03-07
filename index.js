e4ce7d697e05a967c11bc497e65b8b3e01cbfa89
Bhushan Dharne <117301779+bdharne09@users.noreply.github.com>
2023-03-07 23:51:09 +0530

    initial upload

---
+++ index.js
@@ -0,0 +1,35 @@
+var randomNumber1 = Math.random();
+
+randomNumber1 = (randomNumber1 * 6) + 1;
+
+randomNumber1 = Math.floor(randomNumber1);
+
+console.log(randomNumber1);
+
+var img1Str = "images/dice" + randomNumber1 + ".png";
+
+document.getElementsByClassName("img1")[0].setAttribute("src", img1Str);
+
+var randomNumber2 = Math.random();
+
+randomNumber2 = (randomNumber2 * 6) + 1;
+
+randomNumber2 = Math.floor(randomNumber2);
+
+console.log(randomNumber2);
+
+var img2Str = "images/dice" + randomNumber2 + ".png";
+
+document.getElementsByClassName("img2")[0].setAttribute("src", img2Str);
+
+if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "player 2 wins!!";
+
+}
+else if (randomNumber1 > randomNumber2) {
+
+    document.querySelector("h1").innerHTML = "player 1 wins!!";
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw!!";
+}
